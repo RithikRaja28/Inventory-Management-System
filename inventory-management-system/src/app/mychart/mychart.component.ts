873eec9008135903804bf0e3e4602108aa4e672e
rmkec111722203084 <rithikraja28.rr@gmail.com>
2024-02-27 07:43:08 +0530

    Added aditional chat - Stocks in inventory

--- inventory-management-system/src/app/mychart/mychart.component.ts
+++ inventory-management-system/src/app/mychart/mychart.component.ts
@@ -55,5 +55,48 @@ export class MychartComponent implements OnInit {
         },
       },
     });
+    const myChart1 = new Chart('barchart', {
+      type: 'bar',
+      data: {
+        labels: [
+          'Mac Book Pro',
+          'Apple Vision Pro',
+          'Meta Ocules Quest',
+          'Samsung S22',
+          'Logistics',
+          'Iphone 11',
+        ],
+        datasets: [
+          {
+            label: '# of products',
+            data: [550, 167, 250, 600, 50, 75],
+            backgroundColor: [
+              'rgba( 5,  99, 132, 0.2)',
+              'rgba(5, 162, 235, 0.2)',
+              'rgba( 5,206, 86, 0.2)',
+              'rgba( 5,192, 192, 0.2)',
+              'rgba(5, 102, 255, 0.2)',
+              'rgba(5, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
   }
 }
